5670bd47bcd5d7fe85f165d65e9c01584bf30616
FaresAlahmadi <fares.madred11@gmail.com>
2023-11-23 19:02:37 +0300

    Add comment code

--- src/Passenger.java
+++ src/Passenger.java
@@ -1,3 +1,19 @@
 public class Passenger extends Person {
 
-}
+        public static void main(String[] args) {
+            /** int currentFlor;
+             int distinationFlor;
+             long requestTime;
+             long arriveTime ;
+             public void Passenger(){ }
+             public void requestUp(){ }
+             public void requestDown(){ }
+             public String getName(){ }
+             public int getId(){ }
+             public double getWeight(){ }
+             */
+
+        }
+    }
+
+
