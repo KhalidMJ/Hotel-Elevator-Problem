675770f58010df2dfd142c1489da7900afe94aa6
AmmarAlwsabi <xxtammartxx@gmail.com>
2023-11-23 18:59:52 +0300

    Added new lines

--- src/ControlButtons.java
+++ src/ControlButtons.java
@@ -1,2 +1,23 @@
+
+// ControlButtons class has these attributes:
 public abstract class ControlButtons {
+        int ButtonsCount;
+        boolean buttonsStatus;
+
+        // this method return the number of control buttons
+        public int getButtonsCount() {
+            return ButtonsCount;
+        }
+
+        // this method will return the buttons' status
+        public boolean getButtonsStatus() {
+            return buttonsStatus;
+        }
+        // this method to clear buttons
+        public void ClearButtons(){
+            return;
+        }
+    }
+
+
 }
